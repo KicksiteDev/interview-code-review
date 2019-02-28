554ee93d29ca10f7fe9500b3f9bb1d2fc5bb543e
Lee DeBoom <lee@kicksite.net>
2019-02-28 11:32:26 -0600

    Added ability to view post history and post new posts
    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
+  post '/posts/create', to: 'posts#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
